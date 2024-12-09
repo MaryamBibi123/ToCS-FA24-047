245100e5c542e37019b6729329339137ae9174c1
Maryam Bibi <102379865+MaryamBibi123@users.noreply.github.com>
2024-12-09 14:04:27 +0500

    Create SquareCalculator.java

---
+++ SquareCalculator.java
@@ -0,0 +1,8 @@
+public class SquareCalculator {
+    public static void main(String[] args) {
+        double number = 5; 
+        double square = number * number;
+
+        System.out.println("The square of " + number + " is: " + square);
+    }
+}
